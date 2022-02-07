ad549b3bf562a522e6725763a514150db55df574
Bartosz Wilki <b.wilki0109@gmail.com>
2022-02-07 19:38:28 +0100

    17.6

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,11 +1,24 @@
 import styles from './SearchForm.module.scss';
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addSearchPhrase } from '../../redux/store';
+
 
 const SearchForm = () => {
+  const dispatch = useDispatch();
+
+  const [title, setTitle] = useState('')
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    dispatch(addSearchPhrase({ title }));
+  }
+
   return (
-    <form className={styles.searchForm}>
-    <TextInput placeholder="Search..." />
+    <form className={styles.searchForm} onSubmit={handleSubmit}>
+    <TextInput value={title} onChange={e => setTitle(e.target.value)} placeholder="Search..." />
     <Button><span className="fa fa-search" /></Button>
   </form>
   )
